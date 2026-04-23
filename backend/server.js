1d1feb6cab66be0e42c8dfc9f42bb050e13e2b64
nshu15 <shuning010506@gmail.com>
2026-04-23 16:57:34 -0700

    feat(server): initialize express app with mongodb and cheese routes

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+//entry point of application, backend talk to mongoose, frontend fetch 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routerPath = './routes/cheeses.js';
+const cheeseRouter = require(routerPath)
+require('dotenv').config({ path: '../.env' });
+
+const app = express();
+const PORT = process.env.PORT || 3000; //3000 is fallback
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error: ', err));
+
+//middleware
+app.use(cors()); //allows frontend to talk to this 
+app.use(express.json()); //allows server to accept json in request
+
+//routes, prefix all routes in cheeseRouter with '/api/cheeses'
+app.use('/api/cheeses', cheeseRouter)
+
+app.get('/', (req,res) => {
+    res.send('Cheese API is running')
+});
+
+//start server 
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
